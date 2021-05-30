1ba1b70fc4f74ec2555a7413b0c9ed7a681c82ac
Mikael Muniz Ribeiro <65512888+LichtDDR@users.noreply.github.com>
2021-05-30 18:10:57 -0300

    Update App.js
    Updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 const employee = {
-  name: "Mikael",
+  name: "Mikael Muniz Ribeiro",
   job: "Programmer",
   age: "20",
+  datas: {
+    salary: "$100"
+  },
   address: {
     state: "São Paulo",
     number: "11960531167",
@@ -13,14 +16,15 @@ const employee = {
 }
 
 function App() {
-  const { name, job, address } = employee;
+  const { name, job, address, datas } = employee;
   const { state, country, number } = address;
+  const { salary: money } = datas;
   return (
     <div className="App">
       <header className="App-header">
       <h2>{name}</h2>
-      <p>Charge:{job}</p>
-      <p>{`Lives in ${state}, ${country} and his number is ${number}`}</p>
+      <p>Charge: {job}</p>
+      <p>{`Lives in ${state}, ${country} and his number is ${number} and salary ${money}`}</p>
       <p>I'm justing learning array destruturing :)</p>
       </header>
     </div>
